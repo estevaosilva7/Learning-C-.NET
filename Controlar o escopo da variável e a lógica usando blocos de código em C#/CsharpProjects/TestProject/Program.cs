4a7fb678cfe29841158cd7351abcceb9bb76aa46
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2023-12-19 21:21:23 -0300

    Controlar o escopo da variável e lógica em blocos
    Controlar o escopo da variável e a lógica usando blocos de código em C#

---
+++ Controlar o escopo da variável e a lógica usando blocos de código em C#/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,83 @@
+﻿bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    value = 10;
+    Console.WriteLine("Inside of code block: " + value);
+}
+Console.WriteLine("Outside of code block: " + value);
+
+
+bool flag = true;
+int value;
+
+if (flag)
+{
+    value = 10;
+    Console.WriteLine("Inside of code block: " + value);
+}
+Console.WriteLine($"Outside of code block: {value}");
+
+
+bool flag = true;
+if (flag)
+    Console.WriteLine(flag);
+
+bool flag = true;
+if (flag) Console.WriteLine(flag);
+
+string name = "steve";
+if (name == "bob") Console.WriteLine("Found Bob");
+else if (name == "steve") Console.WriteLine("Found Steve");
+else Console.WriteLine("Found Chuck");
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+       bool found = true;
+
+    }
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
+
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
